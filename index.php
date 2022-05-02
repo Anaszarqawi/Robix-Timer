d61e1b433bee2266693ce2ae9a55e5345ec6ad43
ANAS ZARQAWI <75158182+Anaszarqawi@users.noreply.github.com>
2022-05-02 12:02:27 +0200

    add a timer

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Robix Timer</title>
+    <style>
+        <?php include './scss/timer/style.css' ?>
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="scramble"></div>
+        <div class="timer">00 : 00 . 00</div>
+    </div>
+    <script>
+        <?php include './js/packages/jquery-3.6.0.min.js' ?>
+        <?php include './js/script.js' ?>
+    </script>
+</body>
+
+</html>
