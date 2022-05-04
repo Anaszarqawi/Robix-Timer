f6138a8b96dc5161f44e40377711e17d7c2132ef
ANAS ZARQAWI <75158182+Anaszarqawi@users.noreply.github.com>
2022-05-05 00:36:30 +0200

    add a menu/nav/searchBar/other..

---
+++ php/nav.php
@@ -0,0 +1,8 @@
+    <div class="nav">
+        <div class="logo">Rubix Timer</div>
+        <ul class="menu">
+            <li class="item">Profile</li>
+            <li class="item scoresMenu">Menu</li>
+            <!-- <li class="item">Profile</li> -->
+        </ul>
+    </div>
