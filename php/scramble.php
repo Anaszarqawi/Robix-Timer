b45d4c1421aedde6aec3b5c7b045877fe92438a2
ANAS ZARQAWI <75158182+Anaszarqawi@users.noreply.github.com>
2022-05-07 00:29:39 +0200

    add scoreViewer/editScramble

---
+++ php/scramble.php
@@ -0,0 +1,5 @@
+<div class="scrambleContainer">
+    <?php include './assets/svg/edit-icon.svg' ?>
+    <div class="scramble" contenteditable="true">Generating....</div>
+    <?php include './assets/svg/refresh-icon.svg' ?>
+</div>
